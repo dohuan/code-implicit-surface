ed75c51db00a81b183b1462c815d37fdb9116bc7
dohuan <dohuan@msu.edu>
2015-06-18 15:39:39 -0400

    061915 3:39pm

--- Configuration.m
+++ Configuration.m
@@ -17,8 +17,8 @@ function option = Configuration()
     
     option.ifStand = 1; % 0: not standardize data, 1: standardize data
     
-    option.band_x = 1.3;
-    option.band_y = 1.3;
-    option.band_z = 1.3;
+    option.band_x = 1.1;
+    option.band_y = 1.1;
+    option.band_z = 1.1;
     option.band_f = 1;
 end
